b6536089228a7ba31ac688d787053832748f7b6f
dganzha <dmitry.ganzha@cpcs.ws>
2018-07-16 11:50:48 +0300

    create module

---
+++ rxservicegenerator/src/main/java/returnt/ru/rxservicegenerator/Main.java
@@ -0,0 +1,17 @@
+package returnt.ru.rxservicegenerator;
+
+import android.util.Log;
+
+/**
+ * File Main
+ *
+ * Author d.a.ganzha
+ * Project rx-service-generator
+ * Created by 16.07.18 11:49
+ */
+public class Main {
+
+    public void test(){
+        Log.d("dddddddd", "test: 5555555555555555555555555555555555555");
+    }
+}
